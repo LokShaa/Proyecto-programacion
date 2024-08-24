337a25d0f969d46a0b151b343d104b84bc547af7
tonTomas0606 <tvalderrama23@alumnos.utalca.cl>
2024-08-24 13:39:58 -0400

    se solucionaron unos problemas y se agrega la primera funcion para simular la energia en las columnas

--- src/Protoboard.java
+++ src/Protoboard.java
@@ -1,32 +1,37 @@
 import javafx.scene.layout.Pane;
 
- 
-public class Protoboard {
-    public void inicializarMatriz(int rows, int cols, double cellAncho, double cellAlt, double padding1, double padding2, Pane matrizPane, Pane[][] matrix){
-    matrix = new Pane[rows][cols];
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            Pane cell = new Pane();
-            cell.setPrefSize(cellAncho, cellAlt);
-            cell.setStyle("-fx-border-color: black; -fx-border-width: 1; -fx-background-color: black;");//Establece el color de fondo a negro
-            // Calcular la posición de la celda
-            double x = j * (cellAncho + padding1);
-            double y = i * (cellAlt + padding2);
-            cell.setLayoutX(x);
-            cell.setLayoutY(y);
-            matrizPane.getChildren().add(cell);
-            matrix[i][j] = cell;
-            // Añadir evento a cada celda
-            cell.setOnMouseClicked(event -> {
-                //AQUI IRAN LOS METODOS PARA HACER LAS INTERACCIONES DE LA MATRIZ\
-                cell.setStyle("-fx-background-color: purple;");
+public class Protoboard{
+    //Declara matrix como atributo de la clase
+    private Pane[][] matriz;
+
+    public void inicializarMatriz(int filas, int columnas, double cellAncho, double cellAlt, double padding1, double padding2, Pane matrizPane) {
+        matriz = new Pane[filas][columnas];
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                Pane cell = new Pane();
+                cell.setPrefSize(cellAncho, cellAlt);
+                cell.setStyle("-fx-border-color: black; -fx-border-width: 1; -fx-background-color: black;");
                 
-            });
+                // Calcular la posición de la celda
+                double x = j * (cellAncho + padding1);
+                double y = i * (cellAlt + padding2);
+                cell.setLayoutX(x);
+                cell.setLayoutY(y);
+                matrizPane.getChildren().add(cell);
+                matriz[i][j] = cell;
+                
+                // Añadir evento a cada celda
+                final int col = j; // Necesario para usar dentro del lambda
+                cell.setOnMouseClicked(event -> {
+                    // Cambiar el color de toda la columna
+                    for (int row = 0; row < filas; row++) {
+                        matriz[row][col].setStyle("-fx-border-color: black; -fx-border-width: 1; -fx-background-color: blue;"); // Cambiar color a azul
+                    }
+                });
+            }
         }
+        
+        // Ajustar el tamaño del pane matrizPane
+        matrizPane.setPrefSize(columnas * (cellAncho + padding1), filas * (cellAlt + padding2));
     }
-     // Ajustar el tamaño del pane matrizPane
-     matrizPane.setPrefSize(cols * (cellAncho + padding1), rows * (cellAlt + padding2));
-    }
-
 }
-
