1832a8868022c586e93d3ac091a0e6f15f4beced
LokShaa <abecerra23@alumnos.utalca.cl>
2024-11-07 00:47:11 -0300

    Se genera el display, se extrae alguna logica de chip (el hacer el rectangulo y que sea movible). Se puede hacer que prenda el led respectivo a la fila que le corresponda.

--- src/Main.java
+++ src/Main.java
@@ -336,6 +336,10 @@ public class Main extends Application{
         imagenDisplay.setOnMouseExited(exitEvent -> {
             imagenDisplay.setEffect(null);
         });
+        
+        imagenDisplay.setOnMouseClicked(exitEvent -> {
+            Display display = new Display(matrizPane, 100, 200,matrizCentralProtoboard.getMatriz(),matrizCentralProtoboard.getMatrizEnteros());
+        });
     }
 
     private void configurarEventosDeDibujoCablesProtoboard(List<Pane> matrices, Runnable onComplete) {
